85b1fd035be73ab79bb587056c06968a39d4aeac
leon <kelg.fu@gmail.com>
2020-02-28 14:45:17 -0500

    replace white space.

--- RefactorCode/src/main/java/com/refactor/belatrix/BuildLog.java
+++ RefactorCode/src/main/java/com/refactor/belatrix/BuildLog.java
@@ -12,19 +12,19 @@ public class BuildLog {
     StringBuilder sb = new StringBuilder();
 
     if (logError) {
-      sb.append(Constants.LEVEL_LOG_ERROR).append(" ")
+      sb.append(Constants.LEVEL_LOG_ERROR).append(Constants.WHITE_SPACE)
           .append(DateFormat.getDateInstance(DateFormat.LONG).format(new Date())).append(Constants.WHITE_SPACE)
           .append(messageText).append(Constants.NEW_LINE);
     }
 
     if (logWarning) {
-      sb.append(Constants.LEVEL_LOG_WARNING).append(" ")
+      sb.append(Constants.LEVEL_LOG_WARNING).append(Constants.WHITE_SPACE)
           .append(DateFormat.getDateInstance(DateFormat.LONG).format(new Date())).append(Constants.WHITE_SPACE)
           .append(messageText).append(Constants.NEW_LINE);
     }
 
     if (logMessage) {
-      sb.append(Constants.LEVEL_LOG_MESSAGE).append(" ")
+      sb.append(Constants.LEVEL_LOG_MESSAGE).append(Constants.WHITE_SPACE)
           .append(DateFormat.getDateInstance(DateFormat.LONG).format(new Date())).append(Constants.WHITE_SPACE)
           .append(messageText).append(Constants.NEW_LINE);
     }
